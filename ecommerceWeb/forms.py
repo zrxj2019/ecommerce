04eea59595097d2a7c205cda880e2c5ac281767a
phoebe <pheobelai1997@outlook.com>
2019-11-04 16:25:39 +0800

    考试模块（缺查看试卷）+通知模块（待优化模版）

--- ecommerceWeb/forms.py
+++ ecommerceWeb/forms.py
@@ -1,5 +1,10 @@
 from django import forms
 from captcha.fields import *
+from . import models
+
+
 class LoginForm(forms.Form):
     username=forms.CharField(label="学号/教工号",max_length=15,widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': "学号/教工号",'autofocus': ''}))
     password=forms.CharField(label="密码",max_length=20, widget=forms.PasswordInput(attrs={'class': 'form-control','placeholder': "密码"}))
+
+
